ab4a30083d8601036e3f3f73d5b6228bd9d29474
Bocong Zhao <zhaobocong@hotmail.com>
2017-07-18 14:56:46 -0500

    move background image css into index css file

--- app/component/Home.js
+++ app/component/Home.js
@@ -1,17 +1,17 @@
 var React = require('react');
 var ZipCode = require('./ZipCode');
 
-var styles = {
-    background : {
-        backgroundImage:"url('app/images/pattern.svg')"
-    }
-}
+//var styles = {
+    //background : {
+        //backgroundImage:"url('app/images/pattern.svg')"
+    //}
+//}
 
 class Home extends React.Component {
 
     render() {
         return (
-            <div className='home-container' style={styles.background}>
+            <div className='home-container' >
                 <h1>Enter a City and State</h1>
                 <ZipCode direction='column' onSubmitZipCode={
                     (city) => {
