0bf9db66b8a9c1df746c6abc76ab507a768b715b
Bocong Zhao <zhaobocong@hotmail.com>
2019-04-24 15:44:53 -0400

    Fixed api bugs

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,30 +1,31 @@
 var axios = require('axios');
+var _APIKEY = require('./key');
 
-var _APIKEY = '1f6aa4993b868fc4ff34cb359240deb2';
-var _currentWeatherURLBefore = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var _currentWeatherURLBefore =
+  'http://api.openweathermap.org/data/2.5/weather?q=';
 var _currentWeatherURLAfter = '&type=accurate&APPID=';
-var _forecastWeatherURLBefore = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+var _forecastWeatherURLBefore =
+  'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
 var _forecastWeatherURLAfter = '&type=accurate&APPID=';
 
 function getCurrentWeather(city) {
-    var url = _currentWeatherURLBefore + city + _currentWeatherURLAfter + _APIKEY;
+  var url = _currentWeatherURLBefore + city + _currentWeatherURLAfter + _APIKEY;
 
-    return axios.get(url)
-        .then(function(currentWeatherData){
-            return currentWeatherData.data;
-        });
+  return axios.get(url).then(function(currentWeatherData) {
+    return currentWeatherData.data;
+  });
 }
 
 function getForecastWeather(city) {
-    var url = _forecastWeatherURLBefore + city + _forecastWeatherURLAfter + _APIKEY;
+  var url =
+    _forecastWeatherURLBefore + city + _forecastWeatherURLAfter + _APIKEY;
 
-    return axios.get(url)
-        .then(function(forecastWeatherData){
-            return forecastWeatherData.data;
-        });
+  return axios.get(url).then(function(forecastWeatherData) {
+    return forecastWeatherData.data;
+  });
 }
 
 module.exports = {
-    getCurrentWeather : getCurrentWeather,
-    getForecastWeather: getForecastWeather
-}
+  getCurrentWeather: getCurrentWeather,
+  getForecastWeather: getForecastWeather
+};
---
+++ app/utils/key.js
@@ -0,0 +1,3 @@
+var _APIKEY = '1f6aa4993b868fc4ff34cb359240deb2';
+
+module.exports = _APIKEY;
